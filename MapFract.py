af00163c1ece922811b6418650e5b062ba175b28
maewec <nemtsev@numr.org>
2024-02-13 08:55:21 +0300

    Начал описывать вывод результатов измерений

--- MapFract.py
+++ MapFract.py
@@ -41,9 +41,12 @@ def loadfile():
 
 
 class Toolbar:
-    def __init__(self, master, row, column, image_frame):
+    def __init__(self, master, row, column, columnspan, 
+                 image_frame, result):
         # фрейм с изображением
         self.imgf = image_frame
+        # фрейм с результатами
+        self.result = result
         # отступ по умолчанию
         padx = 2
         pady = 2
@@ -51,22 +54,23 @@ class Toolbar:
         self.list_points_measure = []
 
         self.frame = tk.Frame(master)
-        self.frame.grid(row=row, column=column, sticky='nw',
+        self.frame.grid(row=row, column=column, columnspan=columnspan, sticky='nw',
                         padx=padx, pady=pady)
 
         ##################
         # Фрейм информации
         self.frame_info = tk.LabelFrame(self.frame, text='Информация')
-        self.frame_info.grid(row=0, column=0, sticky='nw',
+        self.frame_info.grid(row=0, column=0, sticky='nsw',
                              padx=padx, pady=pady)
         self.text_info = 'x={}\ny={}'
-        self.label_info = tk.Label(self.frame_info, text=self.text_info.format(0, 0))
-        self.label_info.grid(row=0, column=0)
+        self.label_info = tk.Label(self.frame_info, text=self.text_info.format(0, 0),
+                anchor='w')
+        self.label_info.grid(row=0, column=0, sticky='nw')
         # забиндить отображение координат на изображении
         self.canv_bind_info_coord = self.imgf.canv.bind('<Motion>', self.info_coord,
                                                         add=True)
-        self.label_info_len = tk.Label(self.frame_info, text='')
-        self.label_info_len.grid(row=1, column=0)
+        self.label_info_len = tk.Label(self.frame_info, text='', anchor='w')
+        self.label_info_len.grid(row=1, column=0, sticky='nw')
         
         ################
         # Фрейм масштаба
@@ -78,13 +82,13 @@ class Toolbar:
         self.entry_scale_pixels.insert(0, 1)
         self.entry_scale_pixels.bind('<Return>', lambda event: self.get_multiplier())
         self.entry_scale_pixels.grid(row=0, column=0)
-        tk.Label(self.frame_scale, text='px').grid(row=0, column=1)
+        tk.Label(self.frame_scale, text='px', anchor='w').grid(row=0, column=1)
         # поле ввода единиц длины
         self.entry_scale_len = tk.Entry(self.frame_scale, width=8)
         self.entry_scale_len.insert(0, 1)
         self.entry_scale_len.bind('<Return>', lambda event: self.get_multiplier())
         self.entry_scale_len.grid(row=1, column=0)
-        tk.Label(self.frame_scale, text='').grid(row=1, column=1)
+        tk.Label(self.frame_scale, text='', anchor='w').grid(row=1, column=1)
         # кнопка определения числа пикселей
         self.button_scale_pixels = tk.Button(self.frame_scale, text='Измерить шкалу')
         self.button_scale_pixels.bind('<Button-1>',
@@ -92,7 +96,7 @@ class Toolbar:
         self.button_scale_pixels.grid(row=2, column=0, columnspan=2,
                                       sticky='nwe')
         # вывод перевода пикселей в длину
-        self.label_scale = tk.Label(self.frame_scale)
+        self.label_scale = tk.Label(self.frame_scale, anchor='w')
         self.label_scale.grid(row=3, column=0, columnspan=2)
 
         #################
@@ -136,8 +140,7 @@ class Toolbar:
             x = self.imgf.canv.canvasx(event.x)
             y = self.imgf.canv.canvasy(event.y)
             self.list_points_measure.append([x, y])
-            if type_measure != 'draw_distance':
-                self.set_measure(type_measure)
+            self.set_measure(type_measure)
             # отвязываем функцию и обнуляем список
             self.imgf.canv.unbind('<Button-1>', self.canv_bind_measure)
             self.list_points_measure = []
@@ -155,15 +158,20 @@ class Toolbar:
             # забиваем в поле ввода
             self.entry_rotate_angle.delete(0, 'end')
             self.entry_rotate_angle.insert(0, '{:6.2f}'.format(angle))
-        elif type_measure == 'scale':
-            l = math.sqrt((x2-x1)**2+(y2-y1)**2)
-            l = round(l)
-            # забиваем в поле ввода
-            self.entry_scale_pixels.delete(0, 'end')
-            self.entry_scale_pixels.insert(0, l)
-            self.get_multiplier()
         else:
-            raise KeyError(f'Ключ {type_measure} не поддерживается')
+            px = math.sqrt((x2-x1)**2+(y2-y1)**2)
+            px = round(px)
+            multiplier = self.get_multiplier()
+            length = px*multiplier
+            if type_measure == 'scale':
+                # забиваем в поле ввода
+                self.entry_scale_pixels.delete(0, 'end')
+                self.entry_scale_pixels.insert(0, px)
+            elif type_measure == 'draw_distance':
+                # переношу результаты в поле результатов
+                self.result.add_result(length, px, self.line)
+            else:
+                raise KeyError(f'Ключ {type_measure} не поддерживается')
 
     def draw_line(self, event, x, y, type_measure):
         x1 = self.imgf.canv.canvasx(event.x)
@@ -173,7 +181,8 @@ class Toolbar:
         if type_measure == 'draw_distance':
             multiplier = self.get_multiplier()
             l = px*multiplier
-            text = '{:5.0f} px\n{:.4f}'.format(px, l)
+            text = '{:.0f} px\n{:.4f}'.format(px, l)
+            # переношу результаты в поле информации
             self.label_info_len.configure(text=text)
         elif type_measure == 'scale':
             text = '{:.0f} px'.format(px)
@@ -185,6 +194,7 @@ class Toolbar:
         multiplier = length / pixels
         self.label_scale.configure(text=f'1 px = {multiplier:.4f}')
         return multiplier
+
 
 class ImageFrame:
     def __init__(self, master, row, column):
@@ -217,14 +227,63 @@ class ImageFrame:
                                              image=self.image_tkinter)
 
 
+class ResultFrame:
+    def __init__(self, master, row, column):
+        # список всех фреймов с результатами
+        self.list_result = []
+        self.frame = tk.Frame(master)
+        self.frame.grid(row=row, column=column, sticky='nw',
+                        padx=3, pady=3)
+
+        self.frame_result = tk.LabelFrame(self.frame, text='Результаты')
+        self.frame_result.grid(row=0, column=0, sticky='nw',
+                               padx=2, pady=3)
+        tk.Label(self.frame_result, text='N, check, length').grid(row=0, column=0)
+
+    def add_result(self, length, pixel, line):
+        res = MeasureFrame(length, pixel, line, self.frame_result)
+        res.draw()
+        self.list_result.append(res)
+
+
+class MeasureFrame:
+    count = 1
+    def __init__(self, length, pixel, line, master):
+        self.id = MeasureFrame.count
+        MeasureFrame.count += 1
+        self.color = 'red'
+        self.length = length
+        self.pixel = pixel
+        self.line = line
+        self.master = master
+
+    def draw(self):
+        self.row = tk.Frame(self.master)
+        self.row.grid(row=self.id, column=0)
+        # номер результата
+        self.label_num = tk.Label(self.row, text='{:2.0f}'.format(self.id))
+        self.label_num.grid(row=0, column=0)
+        # Чекбокс видимости
+        self.var_visible = tk.BooleanVar(self.row)
+        self.var_visible.set(True)
+        self.check_visible = tk.Checkbutton(self.row, text='',
+                variable=self.var_visible, onvalue=True, offvalue=False)
+        self.check_visible.grid(row=0, column=1)
+
+        print(self.id, '-', self.length, '-', self.pixel)
+
+
+
 mainmenu = tk.Menu(root)
 root.config(menu=mainmenu)
 mainmenu.add_command(label='Открыть изображение', command=loadfile)
 
-image_frame = ImageFrame(root, 1, 0)
-toolbar     =    Toolbar(root, 0, 0, image_frame=image_frame)
+result      = ResultFrame(root, 1, 0)
+image_frame =  ImageFrame(root, 1, 1)
+toolbar     =     Toolbar(root, 0, 0, columnspan=2, 
+                          image_frame=image_frame, result=result)
 
-root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(1, weight=1)
 
 root.mainloop()
